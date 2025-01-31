8514f1a4d6b474ca576735f0ade3f75bbb4386cd
Marcello DeSales <marcello.desales@gmail.com>
2025-01-31 11:43:41 -0800

    :bug: introduce codeql error Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 
 #include <QApplication>
 
+bool checkOverflow(unsigned short x, unsigned short y) {
+  // BAD: comparison is always false due to type promotion
+  return (x + y < x);  
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
