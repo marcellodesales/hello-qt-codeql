c0dd05352e6a2f78675509fc8cd0aae7abf62177
Marcello DeSales <marcello.desales@gmail.com>
2025-01-31 12:37:59 -0800

    :bad: Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,17 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+
+void flawed_strdup(const char *input)
+{
+	char *copy;
+
+	/* Fail to allocate space for terminating '\0' */
+	copy = (char *)malloc(strlen(input));
+	strcpy(copy, input);
+	return copy;
+}
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
