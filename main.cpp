ddbb17e36808fe4bfccdfe2d83fc54189ca47e3c
Marcello DeSales <marcello.desales@gmail.com>
2025-01-31 12:36:06 -0800

    :fireworks: Update main.cpp with docs

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 #include <QApplication>
 
+// CodeQL C++ https://docs.github.com/en/code-security/code-scanning/managing-your-code-scanning-configuration/c-cpp-built-in-queries
+// About Query packs: https://docs.github.com/en/code-security/code-scanning/creating-an-advanced-setup-for-code-scanning/customizing-your-advanced-setup-for-code-scanning#using-queries-in-ql-packs
+// C++ Queries: https://docs.github.com/en/code-security/code-scanning/managing-your-code-scanning-configuration/c-cpp-built-in-queries
+// Write CodeQL Queries: https://codeql.github.com/docs/codeql-language-guides/codeql-for-cpp/ 
+
+// Specific Example on addition https://codeql.github.com/codeql-query-help/cpp/cpp-bad-addition-overflow-check/
 bool checkOverflow(unsigned short x, unsigned short y) {
   // BAD: comparison is always false due to type promotion
   return (x + y < x);  
